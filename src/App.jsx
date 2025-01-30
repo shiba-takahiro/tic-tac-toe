800759126c7c296e3c5adcc0e3096b6d783bd176
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-01-30 16:07:34 +0900

    もう一度stateをリフトアップ

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -23,8 +20,7 @@ function Board() {
     } else {
       nextSquares[i] = 'O'
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares)
@@ -57,6 +53,28 @@ function Board() {
   );
 }
 
+function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  )
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -77,4 +95,4 @@ function calculateWinner(squares) {
   return null
 }
 
-export default Board
+export default Game
